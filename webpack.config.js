84246c8ff04918ba9ffe3ac710dd0baa1505be94
Danko Beribak <beribakdanko@gmail.com>
2020-03-08 15:48:32 -0700

    js projects set up with webpack babel and sass

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // 1
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  // 2
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  // 3
+  devServer: {
+    contentBase: './dist'
+  }
+};
