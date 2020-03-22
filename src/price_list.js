cf69561f2476ab705f88485f15685af5c7a73b73
Danko Beribak <beribakdanko@gmail.com>
2020-03-22 16:58:42 -0700

    navbar height when pressed

--- src/price_list.js
+++ src/price_list.js
@@ -1,15 +1,23 @@
 
 const priceButton = document.querySelector("#price_button");
 const priceList = document.querySelector(".price_list");
+const navbar = document.querySelector(".navbar");
+
+priceList.style.display = "none";
+navHeight = navbar.innerHeight;
 
 priceButton.addEventListener('click', (e) => {
-	if(priceList.style.display === "none") {
+	if(priceList.style.display == "none") {
 		priceList.style.display = "block";
-		priceList.style.position = "absolute";
-		priceList.style.top = "200px";
+		// priceList.style.position = "absolute";
+		// priceList.style.top = "150px";
+		navbar.style.overflow = "scroll";
+		navbar.style.height = "100vh";
+		priceList.style.width = "100vw";
 		priceButton.style.color = "rgb(237, 38, 71)";
 	} else {
 		priceList.style.display = "none";
 		priceButton.style.color = "white";
+		navbar.style.height = '' + navHeight;
 	}
 });
