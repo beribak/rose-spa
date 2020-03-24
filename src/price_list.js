068245a477c518eb9be25cc45b2ba6c242eedcbf
Danko Beribak <beribakdanko@gmail.com>
2020-03-24 02:08:18 -0700

    js price list buttons

--- src/price_list.js
+++ src/price_list.js
@@ -1,8 +1,53 @@
 
-// const priceButton = document.querySelector(".price_button");
-// const prices = document.querySelector(".price_card ul");
+const menuBtn = document.querySelectorAll(".menu-button");
+const facialsBtn = document.querySelector(".facial-btn");
+const treatmantsBtn = document.querySelector(".treatmant-btn");
+const wellBeingBtn = document.querySelector(".well-being-btn");
 
-// prices.style.display == "none";
+let combinedArray = document.querySelectorAll(".price_card");
+// combinedArray.push(...facials, ...treatmants, ...wellBeing)
+
+combinedArray.forEach((item) => {
+  item.style.display = "none"
+})
+
+menuBtn.forEach((item) => {
+	item.addEventListener('click', (e) => {
+
+		if(e.target.classList.contains('facial-btn')) {
+			combinedArray.forEach((item) => {
+			  if(item.classList.contains("facial")) {
+			    	item.style.display = "block"
+			    } else {
+			    	item.style.display = "none"
+			    } 
+			})
+	  	}
+
+	  	if(e.target.classList.contains('treatmant-btn')) {
+			combinedArray.forEach((item) => {
+			  if(item.classList.contains("treatmant")) {
+			    	item.style.display = "block"
+			    } else {
+			    	item.style.display = "none"
+			    } 
+			})
+	  	}
+
+	  	if(e.target.classList.contains('well-btn')) {
+			combinedArray.forEach((item) => {
+			  if(item.classList.contains("well-being")) {
+			    	item.style.display = "block"
+			    } else {
+			    	item.style.display = "none"
+			    } 
+			})
+	  	}
+	  // Todo...
+	});
+  // Todo...
+})
+
 
 // priceButton.addEventListener('click', (e) => {
 // 	if(prices.style.display == "none") {
